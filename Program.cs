8527eb215b1435fa754b3e7cea3c570fcfb8fae5
Guilherme Rodrigues Costa <rodriguesguidev@gmail.com>
2024-02-21 22:02:37 -0300

    pares e impares

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿float numeros;
+
+while(true){ 
+
+Console.Write("...........Pares ou Ímpares ?.........\n\n\n");
+Console.Write("Digite qualquer número:");
+numeros = int.Parse(Console.ReadLine());
+
+if(numeros % 2 == 0) {
+    Console.Write("Este número é PAR\n\n"); 
+} else {
+    Console.Write("Este número é IMPAR\n\n");
+}
+
+
+
+Console.Write("Digite 'Sair' para ecerrar ou 'Enter' para continuar:");
+string opcao = Console.ReadLine();
+if(opcao.ToLower() == "Sair")
+{
+    break;    
+}
+}
